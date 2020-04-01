7b53e6b8782075960d45556cea08e0f2190d6159
everto <everto.fariasd@gmail.com>
2020-04-01 13:39:13 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -20,6 +20,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://evertofd:Santiago7274.@cluster0-ygbta.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
